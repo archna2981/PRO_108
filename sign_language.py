fd730c08ba5d7cd3fdcc25ae01fdcf293c661f01
archna2981 <71487798+archna2981@users.noreply.github.com>
2023-01-02 01:25:35 -0800

    Update sign_language.py

--- sign_language.py
+++ sign_language.py
@@ -6,7 +6,7 @@ hands = mp_hands.Hands()
 mp_draw = mp.solutions.drawing_utils
 cap = cv2.VideoCapture(0)
 
-finger_fold_status =[]
+
 finger_tips =[8, 12, 16, 20]
 thumb_tip= 4
 
@@ -24,20 +24,22 @@ while True:
             lm_list=[]
             for id ,lm in enumerate(hand_landmark.landmark):
                 lm_list.append(lm)
-                for tip in finger_tips:
-                    #getting the landmarks tip and drawing blue circle
-                    x,y = int(lm_list[tip].x*w), int(lm_list[tip].y)*h
-                    cv2.circle(img, (x,y), 15, (255, 0, 0), cv2.FILLED)
+                
+            finger_fold_status =[]
+            for tip in finger_tips:
+                #getting the landmarks tip and drawing blue circle
+                x,y = int(lm_list[tip].x*w), int(lm_list[tip].y)*h
+                cv2.circle(img, (x,y), 15, (255, 0, 0), cv2.FILLED)
 
-                    if lm_list[tip].x < lm_list[tip - 3].x:
-                        cv2.circle(img, (x,y), 15, (0, 255, 0), cv2.FILLED)
-                        finger_fold_status.append(True)
-                    else:
-                        finger_fold_status.append(False)
+                if lm_list[tip].x < lm_list[tip - 3].x:
+                   cv2.circle(img, (x,y), 15, (0, 255, 0), cv2.FILLED)
+                   finger_fold_status.append(True)
+                else:
+                   finger_fold_status.append(False)
             
-            if all(finger_fold_status):
-                #checking if the tummb is up
-                if lm_list[thumb_tip].y < lm_list[thumb_tip-1].y < lm_list[thumb_tip-2].y:
+       if all(finger_fold_status):
+          #checking if the tummb is up
+          if lm_list[thumb_tip].y < lm_list[thumb_tip-1].y < lm_list[thumb_tip-2].y:
                     print("LIKE")
                     cv2.putText(img, "LIKE", (20,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 3)
                 #checking if the tummb is down
